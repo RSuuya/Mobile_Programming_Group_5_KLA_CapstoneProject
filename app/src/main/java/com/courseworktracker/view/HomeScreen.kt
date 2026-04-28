eda62bff60848e103df1ba6422e3b8a54764f17e
Turison 31 <turison31@Turisons-MacBook-Air.local>
2026-04-28 12:48:38 +0300

    Updated HomeScreen.kt

--- app/src/main/java/com/courseworktracker/view/HomeScreen.kt
+++ app/src/main/java/com/courseworktracker/view/HomeScreen.kt
@@ -1,17 +1,41 @@
 package com.courseworktracker.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Assignment
 import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Dashboard
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +52,9 @@ import java.util.Date
 @Composable
 fun HomeScreen(
     viewModel: AssignmentViewModel,
-    onAddAssignment: () -> Unit
+    userName: String,
+    onAddAssignment: () -> Unit,
+    onLogout: () -> Unit
 ) {
     val assignments by viewModel.allAssignments.collectAsState()
     
@@ -198,7 +224,14 @@ fun HomePreview() {
             courseCode = "CS102",
             dueDate = Date(),
             isCompleted = true
-        )
+        ),
+                Assignment(
+                id = 3,
+        title = "E-commerce",
+        courseCode = "CS103",
+        dueDate = Date(),
+        isCompleted = true
+    )
     )
     NdejjeCourseworkTrackerTheme(dynamicColor = false) {
         HomeContent(
